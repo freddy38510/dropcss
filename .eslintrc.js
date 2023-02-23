424b20824d1816006023a7b448b1c94edf755e10
Freddy <freddyescobar@hotmail.fr>
2023-02-23 15:45:39 +0100

    chore: migrate to rollup v3

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,10 @@ module.exports = defineConfig({
     'no-cond-assign': ['error', 'except-parens'],
     'prefer-arrow-callback': 0,
     'mocha/prefer-arrow-callback': 2,
+    'import/no-extraneous-dependencies': [
+      'error',
+      { devDependencies: ['.eslintrc.js', './*', 'test/**'] }, // eslint config file, files at root and test folder
+    ],
   },
   overrides: [
     {
